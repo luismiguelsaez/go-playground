5cceb164b99f14ad906170bb5b4281be42245ff2
luismiguel.saez <luismiguelsaez83@gmail.com>
2021-12-30 11:13:22 +0100

    Added redis cli

---
+++ redis/cli/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/go-redis/redis/v8"
+)
+
+func main() {
+
+	ctx := context.Background()
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+
+	err := rdb.Set(ctx, "key", "value", 0).Err()
+	if err != nil {
+		panic(err)
+	}
+
+	val, err := rdb.Get(ctx, "key").Result()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Got value: %v", val)
+
+}
