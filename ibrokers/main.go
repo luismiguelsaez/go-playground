44f5717b2c12f9f055d7c27b4c4e19a2eac248d0
luismiguel.saez <luismiguelsaez83@gmail.com>
2021-12-30 11:13:33 +0100

    Added functions file

--- ibrokers/main.go
+++ ibrokers/main.go
@@ -1,59 +1,10 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"io"
-	"net/http"
 	"os"
-	"regexp"
 	"time"
 )
-
-func getRefCode(token string, queryId string) (string, error) {
-
-	var rcRequest string = "https://gdcdyn.interactivebrokers.com/Universal/servlet/FlexStatementService.SendRequest?t=" + token + "&q=" + queryId + "&v=3"
-	var refCode string = ""
-
-	resp, err := http.Get(rcRequest)
-	if err != nil {
-		return "", errors.New("error while sending request: " + err.Error())
-	}
-
-	httpBody, err := io.ReadAll(resp.Body)
-	resp.Body.Close()
-	if err != nil {
-		return "", errors.New("error while reading response body: " + err.Error())
-	}
-
-	re := regexp.MustCompilePOSIX("<ReferenceCode>(.*)</ReferenceCode>")
-	submatches := re.FindSubmatch(httpBody)
-
-	if len(submatches) > 1 {
-		refCode = string(submatches[1])
-	} else {
-		return "", errors.New("no matching reference code in response body: " + err.Error())
-	}
-
-	return refCode, nil
-}
-
-func getStatement(token string, refCode string) (string, error) {
-	var stRequest string = "https://gdcdyn.interactivebrokers.com/Universal/servlet/FlexStatementService.GetStatement?t=" + token + "&&v=3&q=" + refCode
-
-	resp, err := http.Get(stRequest)
-	if err != nil {
-		return "", errors.New("error while sending request: " + err.Error())
-	}
-
-	httpBody, err := io.ReadAll(resp.Body)
-	resp.Body.Close()
-	if err != nil {
-		return "", errors.New("error while reading response body: " + err.Error())
-	}
-
-	return string(httpBody), nil
-}
 
 func main() {
 
---
+++ redis/cli/go.mod
@@ -0,0 +1,16 @@
+module redisapp
+
+go 1.17
+
+require github.com/go-redis/redis/v8 v8.11.4
+
+require (
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/keegancsmith/rpc v1.3.0 // indirect
+	github.com/stamblerre/gocode v1.0.0 // indirect
+	golang.org/x/mod v0.5.1 // indirect
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+	golang.org/x/tools v0.1.8 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+)
